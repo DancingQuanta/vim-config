286d08f6eb1d1619c2a24df5b71b64cb1e73ece3
DancingQuanta <andytheseeker@gmail.com>
2017-12-20 09:29:16 +0000

    Check if cygwin then use windows conda

--- .config/nvim/venv.sh
+++ .config/nvim/venv.sh
@@ -13,20 +13,27 @@ venv="${XDG_CACHE_HOME:-$HOME/.cache}/vim/venv"
 [ -d "$venv" ] || mkdir -p "$venv"
 # Check if conda is installed
 if [[ -x $(command -v conda) ]]; then
+  # Conda is windows on cygwin so use windows paths
+  if [[ $OSTYPE == "cygwin" ]]; then
+    venv_path="$(cygpath -aw "$venv")"
+  else
+    venv_path="$venv"
+  fi
+
   echo 'PYTHON 2'
   if [ -d "$venv/neovim2" ]; then
     echo 'Updating Python 2 env for vim'
-    conda env update -p "$venv/neovim2" -f environment2.yml
+    conda env update -p "$venv_path/neovim2" -f environment2.yml
   else
     echo 'Generating Python 2 env for vim'
-    conda env create -p "$venv/neovim2" -f environment2.yml
+    conda env create -p "$venv_path/neovim2" -f environment2.yml
   fi
   if [ -d "$venv/neovim3" ]; then
     echo 'Updating Python 3 env for vim'
-    conda env update -p "$venv/neovim3" -f environment3.yml
+    conda env update -p "$venv_path/neovim3" -f environment3.yml
   else
     echo 'Generating Python 3 env for vim'
-    conda env create -p "$venv/neovim3" -f environment3.yml
+    conda env create -p "$venv_path/neovim3" -f environment3.yml
   fi
 else
   [ -d "$venv/neovim2" ] || $vrenv2 "$venv/neovim2"
